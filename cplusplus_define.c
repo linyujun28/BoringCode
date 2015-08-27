d024ecfb62eae9d9f5e07c6f340d995646c84a97
Ryan_is_eating <yexiaorain@gmail.com>
2015-08-28 03:26:07 +0800

    Create cplusplus_define.c

---
+++ cplusplus_define.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define a 1+1
+#define b 1+1
+int main(){
+	if(a!=b){
+		printf("lose\n");
+		return 0;
+	}else if(a-b==0){
+		printf("lose\n");
+		return 0;
+	}
+	printf("win!\n");
+	return 0;
+}
